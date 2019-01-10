cde60235928ab03693f30d38be2b67d1dbe83f96
Dominik Wawrzkiewicz <dominikwawrzkiewicz141@gmail.com>
2019-01-10 21:11:07 +0100

    initial commit

---
+++ car_rental/car_rental/src/main/java/com/psk_project/car_rental/CarRentalApplication.java
@@ -0,0 +1,14 @@
+package com.psk_project.car_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalApplication.class, args);
+	}
+
+}
+
---
+++ car_rental/car_rental/src/test/java/com/psk_project/car_rental/CarRentalApplicationTests.java
@@ -0,0 +1,17 @@
+package com.psk_project.car_rental;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarRentalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
