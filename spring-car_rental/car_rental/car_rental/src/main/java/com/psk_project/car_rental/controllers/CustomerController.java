7abd4eb6bdd3a19ade43cf6b1dff7c9acd38b634
Karol Ślusarz <karol@karol-pc-debian-lenovo.kompikownia>
2019-01-15 21:14:08 +0100

    First attempt to REST API

--- spring-car_rental/car_rental/car_rental/src/main/java/com/psk_project/car_rental/controllers/CustomerController.java
+++ spring-car_rental/car_rental/car_rental/src/main/java/com/psk_project/car_rental/controllers/CustomerController.java
@@ -1,35 +1,17 @@
-package com.psk_project.car_rental.controllers;
+package com.psk_project.car_rental;
 
-import com.psk_project.car_rental.db.Customer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import services.CustomerService;
 
 import java.util.List;
 
 
 @RestController
-@RequestMapping("/customer")
 public class CustomerController {
-    @Autowired
-    private CustomerService customerService;
 
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public List<Customer> getAllUsers() {
-        return customerService.getAllUsers();
+    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+    public String hello() {
+        System.out.println("Hit me!");
+        return "Hello you";
     }
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-   public String greetings(){
-        return "hellollllllll";
-    }
-
-    @RequestMapping(value = "/addcustomer", method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody()
-    public Customer addNewUser(@RequestBody Customer customer) {
-        return this.customerService.addUser(customer);
-    }
 }
