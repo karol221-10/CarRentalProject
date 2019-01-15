0430aea6db779d712c6c23ee2c9014ac46f12862
Karol Ślusarz <karol@karol-pc-debian-lenovo.kompikownia>
2019-01-15 21:40:59 +0100

    Test: return list of workers

--- spring-car_rental/car_rental/car_rental/src/main/java/com/psk_project/car_rental/CarRentalApplication.java
+++ spring-car_rental/car_rental/car_rental/src/main/java/com/psk_project/car_rental/CarRentalApplication.java
@@ -1,16 +1,11 @@
 package com.psk_project.car_rental;
 
-import com.psk_project.car_rental.db_connection.JdbcOracleConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages={
-		"com.psk_project.car_rental.controllers.CustomerController"})
-
+@SpringBootApplication
 public class CarRentalApplication {
 
 	public static void main(String[] args) {
-		JdbcOracleConnection jdbcOracleConnection = new JdbcOracleConnection();
 		SpringApplication.run(CarRentalApplication.class, args);
 
 	}
--- spring-car_rental/car_rental/car_rental/src/main/java/com/psk_project/car_rental/controllers/CustomerController.java
+++ spring-car_rental/car_rental/car_rental/src/main/java/com/psk_project/car_rental/controllers/CustomerController.java
@@ -1,17 +1,22 @@
-package com.psk_project.car_rental;
+package com.psk_project.car_rental.controllers;
 
+import com.psk_project.car_rental.db.Worker;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/hello")
-    public String hello() {
+    public List<Worker> hello() {
         System.out.println("Hit me!");
-        return "Hello you";
+        List<Worker> works = new ArrayList<Worker>();
+        works.add(new Worker());
+        works.add(new Worker());
+        return works;
     }
-
 }
