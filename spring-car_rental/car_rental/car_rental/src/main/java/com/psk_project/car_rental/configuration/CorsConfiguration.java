53b2b73ee2511f2c013d77d211b229221ed3554e
karol <karol@karol-pc-debian.kompikownia>
2019-01-20 16:01:56 +0100

    Added CorsConfiguration

--- spring-car_rental/car_rental/car_rental/src/main/java/com/psk_project/car_rental/configuration/CorsConfiguration.java
+++ spring-car_rental/car_rental/car_rental/src/main/java/com/psk_project/car_rental/configuration/CorsConfiguration.java
@@ -1,4 +1,14 @@
 package com.psk_project.car_rental.configuration;
 
-public class CorsConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET","PUT","PATCH","POST","DELETE");
+    }
 }
